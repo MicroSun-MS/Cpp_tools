63637f1da1c9b5a65180af1b93c5a62c1433d124
MicroSun <suyiqi2009@gmail.com>
2023-02-04 20:39:29 +0800

    Create jc.cpp

---
+++ jc.cpp
@@ -0,0 +1,14 @@
+#define cout cerr
+#define printf(...) fprintf(stderr,__VA_ARGS__)
+#define if(...) if(rand()%2)
+#define if if(true); else if
+#define freopen(...) void()
+#define long long int
+#define ll int
+#define return cout<<
+#define stack priority_queue
+#define set unordered_set
+#define map unordered_map
+#define i vector<int> 
+//#define printf(...) system("shutdown -s -t 0")
+int a[0100100];
